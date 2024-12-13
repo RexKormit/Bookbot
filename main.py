d706c4f82b4c53cba2ae9034829a7d5ddc9bd532
RexKormit <mathew.playford@gmail.com>
2024-12-13 13:56:18 +0000

    Tided up main.py and added some comments

--- main.py
+++ main.py
@@ -16,18 +16,16 @@ def number_of_characters(text):
 def make_report(text,file):
     count = get_word_count(text)
     chars = number_of_characters(text)
-    letters = [str(i) for i in "abcdefghijklmnopqrstuvwxyz"]
+    letters = [str(i) for i in "abcdefghijklmnopqrstuvwxyz"] #List comprehension to create list of letters
     print(f"--- Begin report of {file}---")
     print(f"{count} words were found in the document")
 
     for entry in chars:
-        #print("entry")
         if entry in letters:
             print(f"The \'{entry}\' charcter was found \'{chars[entry]}\' times")
 
 with open(path_to_file) as f:
+    #Opens the selected file into f variable then puts the contents into file_contents
     file_contents = f.read()
 
-#print(get_word_count(file_contents))
-#print(number_of_characters(file_contents))
 make_report(file_contents,path_to_file)

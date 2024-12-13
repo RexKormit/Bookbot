a59ac6a7c46bb3c4082bf7fb0ca8fa5e4df559f6
RexKormit <mathew.playford@gmail.com>
2024-12-13 13:35:31 +0000

    Added work count and num of chars functions to main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+path_to_file = "books/frankenstien.txt"
+
+def get_word_count(text):
+    return len(text.split())
+
+def number_of_characters(text):
+    text = text.lower()
+    character_dict = {}
+    for letter in text:
+        if letter in character_dict:
+            character_dict[letter] += 1
+        else:
+            character_dict[letter] = 1
+    return character_dict
+
+with open(path_to_file) as f:
+    file_contents = f.read()
+
+#print(get_word_count(file_contents))
+print(number_of_characters(file_contents))

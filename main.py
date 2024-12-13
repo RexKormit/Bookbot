fc76cf1567dd4adfac8e60be564a19771c8e19dc
RexKormit <mathew.playford@gmail.com>
2024-12-13 13:52:56 +0000

    Added make_report function to main.py

--- main.py
+++ main.py
@@ -13,8 +13,21 @@ def number_of_characters(text):
             character_dict[letter] = 1
     return character_dict
 
+def make_report(text,file):
+    count = get_word_count(text)
+    chars = number_of_characters(text)
+    letters = [str(i) for i in "abcdefghijklmnopqrstuvwxyz"]
+    print(f"--- Begin report of {file}---")
+    print(f"{count} words were found in the document")
+
+    for entry in chars:
+        #print("entry")
+        if entry in letters:
+            print(f"The \'{entry}\' charcter was found \'{chars[entry]}\' times")
+
 with open(path_to_file) as f:
     file_contents = f.read()
 
 #print(get_word_count(file_contents))
-print(number_of_characters(file_contents))
+#print(number_of_characters(file_contents))
+make_report(file_contents,path_to_file)
